b8f6231a98022c32ec63eedb132683c69db48416
Brian Roepke <broepke@gmail.com>
2024-10-20 22:49:53 -0500

    Tidy up the UI in case others want to use

--- Home.py
+++ Home.py
@@ -4,6 +4,14 @@ from streamlit_authenticator.utilities import LoginError
 import yaml
 from yaml.loader import SafeLoader
 
+st.set_page_config(page_title="Home")
+st.title("Streamlit-Authenticator")
+st.write("Multi-Page App")
+st.write("https://github.com/mkhorasani/Streamlit-Authenticator")
+
+st.write("Login with the following credentials:")
+st.code("Username: brian\nPassword: password")
+
 # Load credentials from the YAML file
 with open("config.yaml") as file:
     config = yaml.load(file, Loader=SafeLoader)
@@ -16,22 +24,25 @@ authenticator = stauth.Authenticate(
     config["cookie"]["expiry_days"],
 )
 
+# Store the authenticator object in the session state
+st.session_state["authenticator"] = authenticator
+# Store the config in the session state so it can be updated later
+st.session_state["config"] = config
+
 # Authentication logic
 try:
     authenticator.login()
-    st.session_state["authenticator"] = authenticator
-    st.session_state["config"] = config
 except LoginError as e:
     st.error(e)
 
 
 if st.session_state["authentication_status"]:
-    authenticator.logout(location="sidebar", key="logout-demo-app")
-    st.sidebar.write(f'Welcome *{st.session_state["name"]}*')
-    st.title("Streamlit-Autheticator")
-    st.subheader("Multi-Page App")
-    st.write("https://github.com/mkhorasani/Streamlit-Authenticator")
+    authenticator.logout(location="sidebar", key="logout-demo-app-home")
 elif st.session_state["authentication_status"] is False:
     st.error("Username/password is incorrect")
 elif st.session_state["authentication_status"] is None:
     st.warning("Please enter your username and password")
+
+# Quick check of the session state.
+with st.expander("Session State for Debugging", icon="💾"):
+    st.session_state
--- pages/page_1.py
+++ pages/page_1.py
@@ -1,34 +1,21 @@
 import streamlit as st
-from streamlit_authenticator.utilities import LoginError
 
-# Quick check of the session state.
-st.session_state
+st.set_page_config(page_title="Page 1")
+
+if st.session_state.get("authentication_status") is not None:
+    authenticator = st.session_state.get("authenticator")
+    authenticator.login(location="unrendered", key="authenticator-page-1")
 
 
+if st.session_state == {} or st.session_state["authentication_status"] is None:
+    st.page_link("Home.py", label="Home", icon="🏠")
+    st.warning("Please use the button above to navigate to Home and log in.")
+    st.stop()
 
-try:
-    authenticator = st.session_state["authenticator"]
-    config = st.session_state["config"]
-    authenticator.login(location="unrendered", key="login")
-except LoginError:
-    st.session_state["authentication_status"] = False
-except KeyError:
-    st.session_state['authentication_status'] = None
-    st.warning("Please log in first.")
-except Exception as e:
-    st.error(e)    
-
-    
-    # try:
-    #     authenticator.login()
-    #     st.session_state["authenticator"] = authenticator
-    #     st.session_state["config"] = config
-    # except LoginError as e:
-    #     st.error(e)
-        
-    
-
-# authenticator = st.session_state.authenticator
-# name, _, _ = authenticator.login(location='main')
 else:
     st.title("Page 1")
+    st.write(f"Hello {st.session_state.get('name')}, welcome to Page 2!")
+
+# Quick check of the session state.
+with st.expander("Session State for Debugging", icon="💾"):
+    st.session_state
